72ab1e4196eac3103ce0467e84f2cb59b3cef138
mhellestveit <markus.hellestveit@communicate.no>
2025-05-08 20:34:23 +0200

    refactor: clean up and restructure DI setup for clarity

--- cSharpScraper/Infrastructure/DepedencyInjectionBuilder.cs
+++ cSharpScraper/Infrastructure/DepedencyInjectionBuilder.cs
@@ -1,7 +1,6 @@
 using cSharpScraper.Crawler.HackerOneCrawling;
 using cSharpScraper.Crawler.SubDomainCrawler;
 using cSharpScraper.Reconnaisance.GoogleDorking;
-using cSharpScraper.Reconnaisance.SiteArchive;
 using HtmlAgilityPack;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Console;
@@ -15,13 +14,13 @@ public static class DepedencyInjectionBuilder
     public static ServiceProvider SetupDependencyInjection(CrawlerSettings crawlerSettings)
     {
         var ruleProvider = new LocalFileRuleProvider("/Users/mhellestveit/Documents/BugBounty/1SeleniumScrapyScraper/cSharpSCraperSolution/cSharpScraperTests/public_suffix_list.dat");
-        var _ =ruleProvider.BuildAsync().Result;
+        _ = ruleProvider.BuildAsync().Result;
 
         var serviceCollection = new ServiceCollection()
+            //todo: Use AZ key vault to store credentials
             .AddDbContext<DomainDbContext>(options => 
-                options.UseMySql("server=localhost;port=3307;database=scraper;user=root;password=Passord4321Pass", new MySqlServerVersion(new Version(9,1,0)))
+                options.UseMySql("server=localhost;port=3307;database=scraper;user=root;password=xxxx", new MySqlServerVersion(new Version(9,1,0)))
                     .LogTo(Console.WriteLine, LogLevel.Critical))
-            
             .AddLogging(config =>
             {
                 config.AddConsole(options => { options.FormatterName = nameof(CustomConsoleFormatter); });
@@ -29,38 +28,43 @@ public static class DepedencyInjectionBuilder
                 config.AddConsoleFormatter<CustomConsoleFormatter, ConsoleFormatterOptions>();
                 config.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning); // Suppress SQL logs
             })
-            .AddSingleton(x => new ScopeStorage(crawlerSettings.Url))
+            .AddHttpClient<IUrlAvailabilityChecker, UrlAvailabilityChecker>(client =>
+            {
+                client.Timeout = TimeSpan.FromSeconds(10);
+            }).Services
+            .AddHttpClient<WebsiteCrawler, WebsiteCrawler>(client =>
+            {
+                client.Timeout = TimeSpan.FromSeconds(10);
+                client.DefaultRequestHeaders.Add("Accept-Language", "en-GB,en;q=0.9");
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36");
+                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+                client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                Proxy = new WebProxy("http://127.0.0.1:8080"),
+                UseProxy = true,
+                AllowAutoRedirect = false
+            }).Services
+            .AddSingleton<CrawlerSettings>(x => crawlerSettings)
+            .AddSingleton<ScopeStorage>()
             .AddTransient<DomainDbContext>()
-            .AddTransient(provider => new PageStorage(provider.GetService<ILogger<PageStorage>>(),provider.GetService<DomainDbContext>()))
-            .AddTransient(_ => new WebDriverFactory(_.GetService<ILogger<WebDriverFactory>>()))
-            .AddSingleton(_ => new HttpClientFactory())
-            .AddTransient(_ => new GoogleDorker(_.GetService<WebDriverFactory>()?.CreateGoogleDorkingWebdriver(null)))
+            .AddTransient<PageStorage>()
+            .AddTransient<WebDriverFactory>()
+            .AddTransient(provider => new GoogleDorker(provider.GetService<WebDriverFactory>()?.CreateGoogleDorkingWebdriver(null)))
             .AddTransient<DomainDbContextFactory>()
             .AddTransient<PageStorageFactory>()
-            .AddSingleton(provider => new PageDownloader(provider.GetService<ILogger<PageDownloader>>()))
-            .AddTransient(provider => new DomainService(provider.GetService<DomainParser>()))
-            .AddTransient(provider => new DocParser(new HtmlDocument()))
-            .AddTransient(provider =>
-            {
-                var httpClient = provider.GetService<HttpClientFactory>().GetHttpClient();
-                return new WebsiteCrawler(
-                    provider.GetService<PageStorage>(), new DomainParser(ruleProvider), provider.GetService<ILogger<WebsiteCrawler>>(), 
-                   httpClient, crawlerSettings, provider.GetService<GoogleDorker>(), provider.GetService<ArchivedUrlCollector>(), provider.GetService<PageStorageFactory>(), provider.GetService<WebDriverFactory>()
-                        , provider.GetService<PageDownloader>(), provider.GetService<DomainService>(), provider.GetService<DocParser>(), provider.GetService<PageBatcher>());
-            }).AddSingleton(x => new SublisterSubdomainEnumerator(x.GetService<ILogger<SublisterSubdomainEnumerator>>(), new DomainParser(ruleProvider)))
-            .AddSingleton<ISubdomainEnumerator>(x => x.GetService<SublisterSubdomainEnumerator>())
-            .AddSingleton(provider => new SubdomainCrawler(provider.GetService<WebsiteCrawler>(), 
-                provider.GetService<ILogger<SubdomainCrawler>>(), 
-                provider.GetService<ISubdomainEnumerator>(), 
-                provider.GetService<SubdomainScraperStorage>()))
-            .AddSingleton(provider =>
-            {
-                return new HackerOneCrawler(provider.GetService<WebsiteCrawler>(), provider.GetService<ScopeStorage>(),
-                    provider.GetService<ILogger<HackerOneCrawler>>(), provider.GetService<SubdomainCrawler>());
-            })
-            
-            
-                .BuildServiceProvider();
+            .AddSingleton<PageDownloader>()
+            .AddTransient<DomainParser>()
+            .AddTransient<DomainService>()
+            .AddTransient<HtmlDocument>()
+            .AddTransient<DocParser>()
+            .AddTransient<ISubdomainEnumerator, SublisterSubdomainEnumerator>()
+            .AddTransient<DomainParser>(_ => new DomainParser(ruleProvider))
+            .AddTransient<WebsiteCrawler>()
+            .AddSingleton<SublisterSubdomainEnumerator>()
+            .AddSingleton<SubdomainCrawlerDecorator>()
+            .AddSingleton<HackerOneCrawler>()
+            .BuildServiceProvider();
 
         return serviceCollection;
     }   
