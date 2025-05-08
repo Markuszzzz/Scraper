e78d0394e7173bebc328ca42acca3e7b4b3c8f4e
mhellestveit <markus.hellestveit@communicate.no>
2025-05-08 16:54:42 +0200

    Refactor: Remove unused file

--- cSharpScraper/Crawler/WebCrawler/Models/TempPage.cs
+++
@@ -1,9 +0,0 @@
-namespace cSharpScraper.Crawler.WebCrawler.Models;
-
-public class TempPage
-{
-    public string Url { get; set; }
-    public string Content { get; set; }
-    public IEnumerable<string> UrlsOnPage { get; set; }
-    public DomainInfo ScopeDomain { get; set; }
-}
